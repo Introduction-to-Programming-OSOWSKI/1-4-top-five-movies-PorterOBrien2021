6c4ca1a11a47f9dc9109ff8e98d43c42ea3b17c3
PorterOBrien2021 <824085@mystma.org>
2020-12-17 16:56:21 +0000

    Top Five Movies

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def topFiveMovies(movieOne, movieTwo, movieThree, movieFour, movieFive):
-    return movieOne + movieTwo + movieThree + movieFour + movieFive
+    return movieOne + " " + movieTwo + " " + movieThree + " " + movieFour + " " + movieFive
 
-print (topFiveMovies("Iron man", " Shrek", " Shrek 2", " Infinity War", " Zohan"))
+print (topFiveMovies("Iron man", "Shrek", "Shrek 2", "Infinity War", "Zohan"))
